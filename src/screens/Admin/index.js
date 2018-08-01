68e4941261d100d704e6d271a2ab94a76f8f2af2
Diego Dulval de Freitas <diegodifreitas@gmail.com>
2018-08-01 18:41:34 -0300

    Created sub route

--- src/screens/Admin/index.js
+++ src/screens/Admin/index.js
@@ -1,5 +1,22 @@
 import React from "react";
+import { Switch, Link, Route } from "react-router-dom";
 
-const Admin = props => <h1>Admin</h1>;
+const Home = props => <h1>Home Admin</h1>;
+const Users = props => <h1>Users Admin</h1>;
 
+const Admin = props => {
+  return (
+    <div>
+      <h1>Admin</h1>
+      <p>
+        <Link to="/admin">Home</Link>
+        <Link to="/admin/users">Users</Link>
+      </p>
+      <div>
+        <Route path={`${props.match.path}`} exact component={Home} />
+        <Route path={`${props.match.path}/users`} component={Users} />
+      </div>
+    </div>
+  );
+};
 export default Admin;
