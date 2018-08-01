5714436d3399d8f9acbb31b5b2aee8f1ebf0b26e
Diego Dulval de Freitas <diegodifreitas@gmail.com>
2018-08-01 18:28:17 -0300

    Integrated navigation

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import React, { Component } from "react";
 import { Provider } from "react-redux";
+import { Route, BrowserRouter as Router } from "react-router-dom";
 
 import store from "./redux";
 
 import Header from "./components/Header";
 import "./App.css";
 
+const Home = props => <h1>Home</h1>;
+const Admin = props => <h1>Admin</h1>;
+const Restrict = props => <h1>Restrict</h1>;
+const Login = props => <h1>Login</h1>;
+
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <div className="App">
-          <Header />
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
-        </div>
+        <Router>
+          <div className="App">
+            <Header />
+            <Route exact path="/" component={Home} />
+            <Route  path="/admin" component={Admin} />
+            <Route exact path="/restrito" component={Restrict} />
+            <Route exact path="/login" component={Login} />
+
+            <p className="App-intro">
+              To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
+          </div>
+        </Router>
       </Provider>
     );
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { connect } from "react-redux";
-import ActionCreators from "../redux/actionCreators";
+import { Link } from "react-router-dom";
 
+import ActionCreators from "../redux/actionCreators";
 import logo from "../logo.svg";
 
 const Header = props => (
   <header className="App-header">
     <img src={logo} className="App-logo" alt="logo" />
     <h1 className="App-title">Welcome to React</h1>
+    <p>
+      <Link to="/">Home</Link>
+      <Link to="/admin">Admin</Link>
+      <Link to="/restrito">Restito</Link>
+      <Link to="/login">Login</Link>
+    </p>
   </header>
 );
 
