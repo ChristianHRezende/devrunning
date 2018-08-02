34a45babb3569c09b19a0361a0346d8ce08b69e8
Diego Dulval de Freitas <diegodifreitas@gmail.com>
2018-08-02 11:40:36 -0300

    Implement access control in specific pages

--- src/screens/Admin/index.js
+++ src/screens/Admin/index.js
@@ -1,14 +1,18 @@
 import React from "react";
-import { Link, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import { Link, Route, Redirect } from "react-router-dom";
 
 const Home = props => <h1>Home Admin</h1>;
 const Users = props => <h1>Users Admin</h1>;
 
 const Admin = props => {
+  if (!props.auth.isAuth) return <Redirect to="/login" />;
+  if (props.auth.user.role !== "admin") return <Redirect to="/restrito" />;
   return (
     <div>
       <h1>Admin</h1>
       <p>
+        {JSON.stringify(props.auth)}
         <Link to="/admin">Home</Link>
         <Link to="/admin/users">Users</Link>
       </p>
@@ -19,4 +23,8 @@ const Admin = props => {
     </div>
   );
 };
-export default Admin;
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+export default connect(mapStateToProps)(Admin);
